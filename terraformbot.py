97306ac4b72884c769119d21c4806fbfa76db247
Ryan Sturmer <ryansturmer@gmail.com>
2014-01-20 23:10:31 -0500

    Improve flood performance

--- terraformbot.py
+++ terraformbot.py
@@ -15,20 +15,6 @@ OFFSETS = [
 
 CRAFT_USER = 'ryansturmer'
 
-def get_neighbors(x,y,z, xz=False, xy=False, yz=False):
-    n = [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1),(x,y,z-1),
-                        (x+1,y+1,z),(x+1,y-1,z),(x-1,y+1,z),(x-1,y-1,z), 
-                       (x+1,y,z+1),(x+1,y,z-1),(x-1,y,z+1),(x-1,y,z-1),
-                       (x,y+1,z+1),(x,y+1,z-1),(x,y-1,z+1),(x,y-1,z-1)]
-    if(xz):
-        return [(px,py,pz) for (px,py,pz) in n if py == y]
-    if(xy):
-        return [(px,py,pz) for (px,py,pz) in n if pz == z]
-    if(yz):
-        return [(px,py,pz) for (px,py,pz) in n if px == x]
-    
-    return n
-
 def dist(a,b):
     return math.sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2)
 
@@ -39,12 +25,52 @@ class TerraformBot(CraftBot):
 
     def on_you(self, id, position):
         self.material = 3
-        self.signs = []
-        self.walls = []
-        self.blockflood = set()
+        self.clear_memo()
+        self.talk("Terraformbot ready for action.")        
 
-    def terraform_flood(self, x,y,z,radius):
-        empty_blocks = PLANTS.keys() + [0]
+    def get_neighbors(self,x,y,z, xz=False, xy=False, yz=False):
+        # warning memoziation breaks if you try to mix fixed-plane and full neighbor functions!
+        if (x,y,z) in self.NEIGHBORS_MEMO:
+            return self.NEIGHBORS_MEMO[(x,y,z)]
+
+        n = [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1),(x,y,z-1),
+                            (x+1,y+1,z),(x+1,y-1,z),(x-1,y+1,z),(x-1,y-1,z), 
+                           (x+1,y,z+1),(x+1,y,z-1),(x-1,y,z+1),(x-1,y,z-1),
+                           (x,y+1,z+1),(x,y+1,z-1),(x,y-1,z+1),(x,y-1,z-1)]
+        if(xz):
+            n =  [(px,py,pz) for (px,py,pz) in n if py == y]
+        if(xy):
+            n =  [(px,py,pz) for (px,py,pz) in n if pz == z]
+        if(yz):
+            n =  [(px,py,pz) for (px,py,pz) in n if px == x]
+
+        self.NEIGHBORS_MEMO[(x,y,z)] = n
+        return n
+
+    def flood(self, x,y,z,xz=False,xy=False,yz=False):
+        self.clear_memo()
+        block_type = self.get_block(x,y,z)
+        if block_type == 0:
+            return set()
+        fill_blocks = set()
+        stack = [(x,y,z)]
+        while stack:
+            point = stack.pop()
+            fill_blocks.add(point)
+            n = [x for x in self.get_neighbors(*point, xz=xz,yz=yz,xy=xy) if (x not in fill_blocks) and (self.get_block(*x) == block_type)]
+            stack.extend(n)
+        return fill_blocks
+
+    def clear_memo(self):
+        self.TRAP_MEMO = {}
+        self.NEIGHBORS_MEMO = {}
+
+    def terraform_flood(self, x,y,z,radius,plants=False):
+        self.clear_memo()
+        if plants:
+            empty_blocks = set(PLANTS.keys() + [0])
+        else:
+            empty_blocks = set([0])
 
         # Start block
         block_type = self.get_block(x,y,z)
@@ -59,15 +85,19 @@ class TerraformBot(CraftBot):
         stack = [(x,y,z)]
 
         def is_trapped(x,y,z):
-            neighbors = get_neighbors(x,y,z)
-            return not any(map(lambda x : self.get_block(*x) in empty_blocks, neighbors))
+            if (x,y,z) in self.TRAP_MEMO:
+                return self.TRAP_MEMO[(x,y,z)]
+            neighbors = self.get_neighbors(x,y,z)
+            trapped = not any(map(lambda x : self.get_block(*x) in empty_blocks, neighbors))
+            self.TRAP_MEMO[(x,y,z)] = trapped
+            return trapped
 
         while stack:
             # Block to examine
             point = stack.pop()
             fill_blocks.add(point)
 
-            neighbors = get_neighbors(*point)
+            neighbors = self.get_neighbors(*point)
             for neighbor in neighbors:
                 if neighbor not in fill_blocks and not is_trapped(*neighbor) and xzdist(neighbor, (x,y,z)) <= radius and self.get_block(*neighbor) != 0:
                     stack.append(neighbor)
@@ -106,6 +136,13 @@ class TerraformBot(CraftBot):
             self.remove_sign(x,y,z,face)
             radius = int(args[1])
             self.do_terraform(x,y,z,radius,{1:7,2:56,5:0,15:0,19:18,20:18,21:18,22:18,23:18})
+        elif cmd == 'fillxz':
+            self.remove_sign(x,y,z,face)
+            self.do_fillxz(x,y,z)
+        elif cmd == 'material':
+            self.remove_sign(x,y,z,face)
+            self.talk('is setting material to %s' % MATERIALS.get(block_type, block_type))
+            self.material = block_type
 
     def find_ground(self, x,y,z):
         for y in range(y,0,-1):
@@ -114,8 +151,9 @@ class TerraformBot(CraftBot):
                 return x,y,z
 
     def do_terraform(self, x,y,z, radius, material_map):
+            self.clear_memo()
             self.talk('is calculating a terraform...')
-            blocks = self.terraform_flood(x,y,z,radius)
+            blocks = self.terraform_flood(x,y,z,radius,plants=True)
             self.talk('is terraforming...')
             for x,y,z in blocks:
                 type = self.get_block(x,y,z)
@@ -124,7 +162,18 @@ class TerraformBot(CraftBot):
                     self.remove_block(x,y,z)
                     if material != 0:
                         self.add_block(x,y,z,material, check=False)
-            self.talk('is done.')
+            self.talk('is done.  Processed %s blocks.' % len(blocks))
+
+    def do_fillxz(self, x,y,z):
+        self.clear_memo()
+        self.talk('is calculating a fill...')
+        blocks = self.flood(x,y,z, xz=True)
+        for x,y,z in blocks:
+            self.remove_block(x,y,z)
+            self.add_block(x,y,z,self.material, check=False)
+        self.talk('is done.  Processed %s blocks.' % len(blocks))
+
+
 import sys
 
 bot = TerraformBot(host=sys.argv[1])
